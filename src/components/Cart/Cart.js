c32f9e7d3cfd7e37d9fd07de32d678b94cedc0ab
tarekahmed196 <tarekjustcse@gmail.com>
2023-04-15 12:03:19 +0600

    ternary operator added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart, removeFromCart}) => {
     console.log(cart);
+
+    //conditional rendering
+    let message;
+    if(cart.length===0){
+        message = <p style={{color:'red'}}>Please buy at least one</p>
+    }
+    else{
+        message= <p style={{color:'green'}}>Thanks for buying</p>
+    }
     
     return (
         <div>
-            <h2>Order summary</h2>
+            <h2 className={cart.length===1 ? `purple`: `orange`}>Order summary</h2>
             <h4>Order quantity:{cart.length}</h4>
             
             {
@@ -16,6 +26,9 @@ const Cart = ({cart, removeFromCart}) => {
                     <button onClick={()=>removeFromCart(tshirt)}>X</button>
 
                 </p>)
+            }
+            {
+                message
             }
         </div>
     );
